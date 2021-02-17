a0d4a2433930a49b6d0cc2b18b87e5592b943a36
Jake Readman <jakereadman@gmail.com>
2021-02-17 15:43:16 +0000

    refactor login page

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -192,6 +192,38 @@ function redirect($location) {
     exit;
 }
 
+function loginUser($username, $password) {
+
+    $username = mysqli_real_escape_string($connection, $username);
+    $password = mysqli_real_escape_string($connection, $password);
+
+    $select_user_query = selectStatusQuery('users', 'username', $username);
+
+    while($row = mysqli_fetch_array($select_user_query)) {
+        $db_user_id = escape($row['user_id']);
+        $db_username = escape($row['username']);
+        $db_user_firstname = escape($row['user_firstname']);
+        $db_user_lastname = escape($row['user_lastname']);
+        $db_user_role = escape($row['user_role']);
+        $db_user_password = escape($row['user_password']);
+    }
+
+    // $password = crypt($password, $db_user_password);
+
+
+    if(password_verify($password, $db_user_password)) {
+        $_SESSION['username'] = $db_username;
+        $_SESSION['user_firstname'] = $db_user_firstname;
+        $_SESSION['user_lastname'] = $db_user_lastname;
+        $_SESSION['user_role'] = $db_user_role;
+
+        redirect("../index.php");
+        
+    } else {
+        redirect("../index.php");
+    }
+}
+
 function ifIsMethod($method=null) {
     return $_SERVER['REQUEST_METHOD'] == strtoupper($method);
 }
