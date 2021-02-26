b476095bfd05d22ce2f93617e0b17945d2184287
Jake Readman <jakereadman@gmail.com>
2021-02-26 17:22:28 +0000

    finish footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,8 @@
    <footer>
        <div class="row">
            <div class="col-lg-12">
-               <p class="text-center">Copyright &copy; Jake Readman</p>
+               <p class="text-center">A Jake Readman Webisite</p>
+               <p class="text-center"><a target="_blank" href="http://jakereadman.com">Jakereadman.com</a></p>
            </div>
            <!-- /.col-lg-12 -->
        </div>
